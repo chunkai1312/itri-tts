d0b1672c0bf3567e08feb6c2ebad8959f76bbe2a
Chun-Kai Wang <chunkai1312@gmail.com>
2016-08-31 11:44:05 +0800

    refactor: use standard.js style

--- test/tts_client.test.js
+++ test/tts_client.test.js
@@ -1,19 +1,18 @@
-'use strict';
+'use strict'
 
-var expect = require('chai').expect;
-var tts = require('../')('accountID', 'password');
+var expect = require('chai').expect
+var tts = require('../')('accountID', 'password')
 
 describe('TTSClient', function () {
-
   describe('#ConvertSimple()', function () {
     it('should return result without err', function (done) {
       tts.ConvertSimple('Test the convert simple function', function (err, result) {
-        expect(err).to.not.exist;
-        expect(result).to.exist;
-        done();
-      });
-    });
-  });
+        expect(err).to.not.exist
+        expect(result).to.exist
+        done()
+      })
+    })
+  })
 
   describe('#ConvertText()', function () {
     it('should return result without err', function (done) {
@@ -23,14 +22,14 @@ describe('TTSClient', function () {
         volume: 100,
         speed: 0,
         outType: 'wav'
-      };
+      }
       tts.ConvertText(options, function (err, result) {
-        expect(err).to.not.exist;
-        expect(result).to.exist;
-        done();
-      });
-    });
-  });
+        expect(err).to.not.exist
+        expect(result).to.exist
+        done()
+      })
+    })
+  })
 
   describe('#ConvertAdvancedText()', function () {
     it('should return result without err', function (done) {
@@ -43,23 +42,22 @@ describe('TTSClient', function () {
         PitchLevel: 0,
         PitchSign: 0,
         PitchScale: 5
-      };
+      }
       tts.ConvertAdvancedText(options, function (err, result) {
-        expect(err).to.not.exist;
-        expect(result).to.exist;
-        done();
-      });
-    });
-  });
+        expect(err).to.not.exist
+        expect(result).to.exist
+        done()
+      })
+    })
+  })
 
   describe('#GetConvertStatus()', function () {
     it('should return result without err', function (done) {
       tts.GetConvertStatus(1234567890, function (err, result) {
-        expect(err).to.not.exist;
-        expect(result).to.exist;
-        done();
-      });
-    });
-  });
-
-});
+        expect(err).to.not.exist
+        expect(result).to.exist
+        done()
+      })
+    })
+  })
+})
